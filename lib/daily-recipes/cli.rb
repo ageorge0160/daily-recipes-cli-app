8faa2118bc334c9a05ae72c601a4b53d727fb6c9
Amanda George <github email address>
2017-05-17 21:26:22 -0500

    outlining DailyRecipe code

--- lib/daily-recipes/cli.rb
+++ lib/daily-recipes/cli.rb
@@ -4,6 +4,25 @@ class DailyRecipe
 
   def call
     puts "Today's Meal:"
+    list_daily_meals
+    show_daily_recipe
+  end
+
+  def list_daily_meals
+    puts "PB & J - Sandwich made of peanut butter and jelly"
+  end
+
+
+  def list_staff_picks
+  end
+
+  def show_daily_recipe
+    puts "Would you like the recipe card?"
+    input = gets.strip
+    if input == "y"
+      puts "Recipe Card for PB & J"
+    end
+
   end
 
 end
