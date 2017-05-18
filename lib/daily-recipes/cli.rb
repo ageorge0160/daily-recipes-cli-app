4ead10d4e27c1d0e2bb8cd60e60038b602f483bf
Amanda George <github email address>
2017-05-18 09:27:52 -0500

    combine daily and staff meals in one method

--- lib/daily-recipes/cli.rb
+++ lib/daily-recipes/cli.rb
@@ -3,49 +3,35 @@
 class DailyRecipe
 
   def call
-    puts "Today's Meal:"
+    puts "Today's Meals:"
     list_daily_meals
-    show_daily_recipe
-    list_staff_picks
-  
+    show_recipe_card
+
   end
 
   def list_daily_meals
-    puts "PB & J - Sandwich made of peanut butter and jelly"
+    puts "Meal of the day: PB & J - Sandwich made of peanut butter and jelly"
+    puts "Today's Staff Picks: 1. French Toast 2. Pancakes"
   end
 
-
-  def list_staff_picks
-    puts "Would you like to see today's staff picks?(y/n)"
-    input = gets.strip
-    case input
-      when "y"
-        puts "Today's Staff Picks: 1. French Toast 2. Pancakes 3.Exit"
-        puts "Which recipe card would you like?"
-        input = gets.strip
-        case input
-          when "1"
-            puts "French Toast"
-          when "2"
-            puts "Pancakes"
-          when "3"
-            puts "Goodbye"
-        end
-      when "n"
-        puts "Goodbye"
-    end
-
-  end
-
-
-  def show_daily_recipe
-    puts "Would you like the recipe card?(y/n)"
-    input = gets.strip
-    if input == "y"
-      puts "Recipe Card for PB & J"
+  def show_recipe_card
+    input = nil
+    while input != "exit"
+      puts "Which recipe card would you like?"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Recipe Card for PB & J"
+      when "2"
+        puts "Recipe Card for French Toast"
+      when "3"
+        puts "Recipe Card for Panckaes"
+      when "list meals"
+        list_daily_meals
+      else
+        puts "Please try y/n again"
+      end
     end
   end
-
-
 
 end
