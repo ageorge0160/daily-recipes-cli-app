7b9bdd76228a4a7d742463be7d68df1b42bad583
Amanda George <github email address>
2017-05-18 13:42:08 -0500

    layout of recipes

--- lib/daily-recipes/recipe.rb
+++ lib/daily-recipes/recipe.rb
@@ -2,10 +2,14 @@ class Recipe
 
   attr_accessor :name, :description, :url
 
+  def initialize
+    @name = name
+    @description = description
+    @url = url
+  end
+
   def self.today
     #return many instances of Recipe
-    puts "Meal of the day: 1. Johnsonville Italian Sausage Lasagna"
-    puts "Today's Staff Picks: 2. Pressure Cooker Barbeque Chicken 3. Cornell Chicken"
 
     recipe_1 = self.new
     recipe_1.name = "Johnsonville Italian Sausage Lasagna"
@@ -22,6 +26,17 @@ class Recipe
     recipe_3.name = "Cornell Chicken"
     recipe_3.description = "This famous grilled chicken recipe was created by Dr. Robert C. Baker at New York's Cornell University. They say Dr. Baker was simply trying to invent an easy and delicious way to grill smaller, younger chickens, so that the local chicken farms could sell more birds. The doc's tasty recipe ended up being such a success that sales in the area soared, and the recipe became a statewide favorite."
     recipe_3.url = "http://allrecipes.com/recipe/221091/cornell-chicken/?internalSource=staff%20pick&referringId=1&referringContentType=recipe%20hub&clickId=cardslot%204"
+
+    [recipe_1, recipe_2, recipe_3]
+
+    puts ""
+    puts "-------------- Meal of the day: --------------"
+    puts "1. #{recipe_1.name} - #{recipe_1.description}"
+    puts ""
+    puts "-------------- Today's Staff Picks: --------------"
+    puts "2. #{recipe_2.name} - #{recipe_2.description}"
+    puts ""
+    puts "3. #{recipe_3.name} - #{recipe_3.description}"
 
   end
 
