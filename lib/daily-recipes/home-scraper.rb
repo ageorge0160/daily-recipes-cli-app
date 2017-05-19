ac01ee206acffe9933931be01ee5c13ebcafe93b
Amanda George <github email address>
2017-05-19 16:49:57 -0500

    change website

--- lib/daily-recipes/home-scraper.rb
+++ lib/daily-recipes/home-scraper.rb
@@ -5,7 +5,7 @@ class HomeScraper
   attr_accessor :name, :description, :url, :cardslot
 
   def initialize
-    @doc = Nokogiri::HTML(open("http://allrecipes.com/recipes/"))
+    @doc = Nokogiri::HTML(open("https://recipes.tamouse.org/full-index.html"))
     @name = name
     @description = description
     @url = url
@@ -14,10 +14,11 @@ class HomeScraper
 
   def scrape_tiles
     recipe_today = []
-    @doc.css("article.grid-col--fixed-tiles").each do |tile|
-      recipe_today << {:name => tile.css("h3").text.strip , :description => tile.css("div.rec-card__description").text, :url =>}
+    @doc.css(".col-md-4").each do |tile|
+      recipe_today << {:name => tile.css("h3 a").text.strip , :description => tile.css(".post-excerpt p").text, :url => tile.css("h3 a").attribute("href").value }
     end
     binding.pry
   end
 
 end
+#  "http://allrecipes.com" +
